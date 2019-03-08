39a6e41ce6f7b5df24d9427b952244a09813e173
Kareem Elsawah <alsawahkareem@gmail.com>
2019-03-08 22:17:00 +0200

    Finished Node.js server

--- app.js
+++ app.js
@@ -8,12 +8,11 @@ var phone = require('./routes/phone');
 var app = express();
 
 var fs = require('fs');
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-//app.use('/', controlSettings);
 app.use('/phone', phone);
 app.get("/",function (req,res){
   toSend = currentOrientation[0]+','+currentOrientation[1]+','+currentOrientation[2];
